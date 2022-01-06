4d992e5fb32a3d79f7905f8ce40b4b6a73129158
Isaac Francisco <78627776+isahers1@users.noreply.github.com>
2022-01-06 15:40:34 -0800

    Create homework2.cpp

---
+++ homework2.cpp
@@ -0,0 +1,22 @@
+/*
+ * Part of CS70 Homework 2. This file may NOT be
+ * shared with anyone other than the author(s) and
+ * the current semester's CS70 staff without
+ * explicit written permission from one of the
+ * CS70 instructors.
+ */
+
+#include <cs70/turtle.hpp>
+#include "embroidery.hpp"
+
+int main() {
+    /*
+     * This week main() will just call the functions you will write in
+     * the homework. Uncomment the appropriate lines as you work on each
+     * section of the assignment.
+     */
+
+    meetTurtle();
+    plotExampleData();
+    // plotStudentData();
+}
