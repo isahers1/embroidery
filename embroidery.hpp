454db7f44e97049a9ee5a8cfab2c4f3428f05000
Isaac Francisco <78627776+isahers1@users.noreply.github.com>
2022-01-06 15:41:19 -0800

    Create embroidery.hpp

---
+++ embroidery.hpp
@@ -0,0 +1,14 @@
+
+#include <cs70/turtle.hpp>
+#ifndef EMBROIDERY_HPP_INCLUDED
+#define EMBROIDERY_HPP_INCLUDED
+
+void rect(Turtle& t, float width, float height);
+
+void meetTurtle();
+
+void plotExampleData();
+
+void plotStudentData();
+
+#endif
